ca300609fccee03dd5558cf65411dc171fab4fb2
sudaaher74-ctrl <sudaaher74@gmail.com>
2026-06-23 15:42:53 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom', 'react-helmet-async'],
-          animations: ['gsap', 'lenis']
-        }
-      }
-    },
+
     chunkSizeWarningLimit: 1000
   }
 })
