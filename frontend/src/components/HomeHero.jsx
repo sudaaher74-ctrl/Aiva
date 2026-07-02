048d352c379237e9a47342c8a4210d9c73f01a9c
sudaaher74-ctrl <sudaaher74@gmail.com>
2026-07-03 00:36:30 +0530

    update img

--- frontend/src/components/HomeHero.jsx
+++ frontend/src/components/HomeHero.jsx
@@ -129,7 +129,6 @@ function HomeHero() {
           height="600"
           data-speed="1.8"
         />
-        
         {/* Theme-specific trees */}
         <img
           src="/assets/images/products/mangotree.png"
@@ -138,14 +137,6 @@ function HomeHero() {
           width="800"
           height="1000"
           data-speed="0.8"
-        />
-        <img
-          src="/assets/images/products/bananatree.png"
-          className="float-item f-bananatree theme-orchard-only"
-          alt="Banana Tree Background"
-          width="700"
-          height="900"
-          data-speed="-0.5"
         />
       </div>
     </section>
