1d2d1322159d3b3d6d47534c9df8f7afa18fa9c2
sudaaher74-ctrl <sudaaher74@gmail.com>
2026-07-09 12:05:36 +0530

    remove papaya img

--- frontend/src/components/HomeHero.jsx
+++ frontend/src/components/HomeHero.jsx
@@ -116,14 +116,6 @@ function HomeHero() {
           height="600"
           data-speed="-1.2"
         />
-        <img
-          src="/assets/images/products/pulp/papayapulp.webp"
-          className="float-item f-papaya"
-          alt="Papaya Pulp Supplier"
-          width="400"
-          height="600"
-          data-speed="1.8"
-        />
       </div>
     </section>
   );
