725cb9affa27cf6769634215699bda078ee1f7a2
sudaaher74-ctrl <sudaaher74@gmail.com>
2026-07-29 13:13:06 +0530

    Hide footer on mobile UI

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer hidden md:block">
       <div className="container">
         <div className="footer-top">
           <div className="footer-brand">
